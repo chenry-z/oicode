11cbdd3eccac581d6e89f75b6306d784628308e1
Henry Chen <i@chenryz.top>
2023-03-07 21:00:02 +0800

    add CF1218D

---
+++ Codeforces/1218D.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+template <typename T> T inverse(T a, T m) {
+	T u = 0, v = 1;
+	while (a != 0) {
+		T t = m / a;
+		m -= t * a, std::swap(a, m);
+		u -= t * v, std::swap(u, v);
+	}
+	assert(m == 1);
+	return u;
+}
+template <typename T> class Modular {
+public:
+	using Type = typename std::decay<decltype(T::value)>::type;
+	constexpr Modular() : value() {}
+	template <typename U> Modular(const U& x) {value = normalize(x);}
+	template <typename U> static Type normalize(const U& x) {
+		Type v;
+		if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+		else v = static_cast<Type>(x % mod());
+		if (v < 0) v += mod();
+		return v;
+	}
+	const Type& operator()() const { return value; }
+	template <typename U> explicit operator U() const { return static_cast<U>(value); }
+	constexpr static Type mod() { return T::value; }
+	Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+	Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+	template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+	template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+	Modular operator-() const { return Modular(-value); }
+	template <typename U = T> typename std::enable_if<std::is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+		value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+		return *this;
+	}
+	template <typename U = T> typename std::enable_if<std::is_same<typename Modular<U>::Type, long long>::value, Modular>::type& operator*=(const Modular& rhs) {
+		long long q = static_cast<long long>(static_cast<long double>(value) * rhs.value / mod());
+		value = normalize(value * rhs.value - q * mod());
+		return *this;
+	}
+	template <typename U = T> typename std::enable_if<!std::is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+		value = normalize(value * rhs.value);
+		return *this;
+	}
+	Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+	template <typename U> friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+	template <typename V, typename U> friend V& operator>>(V& stream, Modular<U>& number);
+private:
+	Type value;
+};
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> power(Modular<T> a, U b) {
+	assert(b >= 0);
+	for (Modular<T> res = 1; ; a *= a) {
+		if (b & 1) res *= a;
+		if ((b >>= 1) == 0) return res;
+	}
+}
+template <typename U, typename T> U& operator<<(U& stream, const Modular<T>& number) { return stream << number(); }
+template <typename U, typename T> U& operator>>(U& stream, Modular<T>& number) {
+	typename std::common_type<typename Modular<T>::Type, long long>::type x;
+	stream >> x;
+	number.value = Modular<T>::normalize(x);
+	return stream;
+}
+// using ModType = int;
+// struct VarMod { static ModType value; };
+// ModType VarMod::value;
+// ModType& md = VarMod::value;
+// using Mint = Modular<VarMod>;
+constexpr int md = 1000000007;
+using Mint = Modular<std::integral_constant<std::decay<decltype(md)>::type, md>>;
+std::vector<Mint> fact{1}, inv_fact{1};
+Mint C(int n, int k) {
+	if (k < 0 || k > n) return 0;
+	while ((int) fact.size() < n + 1) {
+		fact.push_back(fact.back() * (int) fact.size());
+		inv_fact.push_back(1 / fact.back());
+	}
+	return fact[n] * inv_fact[k] * inv_fact[n - k];
+}
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	int n, m, sum = 0;
+	std::cin >> n >> m;
+	std::vector<std::vector<std::pair<int, int>>> e(n);
+	constexpr int N = 17;
+	for (int i = 0, u, v, w; i < m; ++i) {
+		std::cin >> u >> v >> w;
+		--u, --v;
+		e[u].emplace_back(v, w);
+		e[v].emplace_back(u, w);
+		sum ^= w;
+	}
+	std::vector<int> dfn(n), fa(n), faw(n);
+	std::vector<std::vector<int>> cycle;
+	auto DFS = [&](auto &&DFS, int u) -> void {
+		static int dfx = 0;
+		dfn[u] = ++dfx;
+		for (auto [v, w] : e[u]) {
+			if (dfn[v] == 0) {
+				fa[v] = u, faw[v] = w;
+				DFS(DFS, v);
+			} else if (dfn[v] > dfn[u]) {
+				cycle.push_back({});
+				int cur = w;
+				for (int now = v; now != u; now = fa[now]) cur ^= faw[now];
+				sum ^= cur;
+				cycle.back().emplace_back(cur ^ w);
+				for (int now = v; now != u; now = fa[now]) cycle.back().emplace_back(cur ^ faw[now]);
+			}
+		}
+	};
+	DFS(DFS, 0);
+	constexpr int inv2 = (md + 1) / 2;
+	auto FWT = [&](std::vector<Mint> &f, bool rev) {
+		for (int i = 0; i < N; ++i)
+			for (int j = 0; j < (1 << N); j += (1 << (i + 1)))
+				for (int k = 0; k < (1 << i); ++k) {
+					Mint a = f[j | k], b = f[(1 << i) | j | k];
+					f[j | k] = a + b, f[(1 << i) | j | k] = a - b;
+					if (rev) f[j | k] *= inv2, f[(1 << i) | j | k] *= inv2;
+				}
+	};
+	std::vector<Mint> A(1 << N);
+	A[sum] = 1;
+	FWT(A, false);
+	for (auto V : cycle) {
+		std::vector<Mint> B(1 << N);
+		for (auto x : V) B[x] += 1;
+		FWT(B, false);
+		for (int i = 0; i < (1 << N); ++i) A[i] *= B[i];
+	};
+	FWT(A, true);
+	for (int i = 0; i < (1 << N); ++i)
+		if (A[i] != 0) return std::cout << i << ' ' << A[i] << '\n', 0;
+	return 0;	
+}
